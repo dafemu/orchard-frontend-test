a1a6df1d3af626e9f9db6d502d0c00167ed5b80a
David Munoz Barco <david.f.do@hotmail.com>
2025-02-25 11:59:42 -0500

    First commit init react project config

--- README.md
+++
@@ -1,2 +0,0 @@
-# orchard-frontend-test
-This is the repository to resolve the test for frontend developer
---
+++ orchard-frontend-test/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
